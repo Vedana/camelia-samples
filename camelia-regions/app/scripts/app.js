5e2330d5671d9bd2c65a6751081b3b1191022204
Olivier Oeuillot <olivier.oeuillot@gmail.com>
2015-01-05 11:00:10 +0100

    Update app.js
    Remove about controller

--- camelia-regions/app/scripts/app.js
+++ camelia-regions/app/scripts/app.js
@@ -19,9 +19,6 @@ angular
 			$routeProvider.when('/', {
 				templateUrl: 'views/main.html',
 				controller: 'MainCtrl'
-			}).when('/about', {
-				templateUrl: 'views/about.html',
-				controller: 'AboutCtrl'
 			}).otherwise({
 				redirectTo: '/'
 			});
